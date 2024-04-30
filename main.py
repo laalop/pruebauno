b0f5e49640c2ca5d6481187fc066a39e0d2e8b16
laalop <168581789+laalop@users.noreply.github.com>
2024-04-30 12:58:13 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -5,12 +5,7 @@ app=Flask(__name__)
 
 @app.route("/")
 def Hola_mundo():
-   mensaje"esta api convierte los dias a horas"
-  return menssaje
+  return "Hola a todos"
 
-@app.route("/Horas/<dias>")
-def conversor_dias_horas(dias):
-    resultado = int(dias)*24
-  return f" (dias) dias son {resultado} horas"
 if __name__=="__main__":
   app.run()
