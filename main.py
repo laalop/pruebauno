cd59f191a93bcca28003c5bcb9bebf7acf204a2e
laalop <168581789+laalop@users.noreply.github.com>
2024-05-03 13:10:09 -0600

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,22 @@ def Principal():
   return "Esta es una API que te muetra el nombre de perros, raza y estatura"
 
 @app.route("/Por_Numero/<Numero>")
-def PorNumero(Numero):
+def Numero(Numero):
   Numero=int(Numero)
   fila=base[base["Numero"]==Numero]
   respuesta=f"El Perro {Numero} es {fila.loc[:,'nombre']}"
   return respuesta
 
-if __name__=="__main__":
-  app:run()
+@app.route("/Por_raza/<raza>")
+def raza(raza):
+  resultados=base[base["raza"]==raza]
+  resultados=srt(resultados)
+  return resultados
+
+@app.route("/Por_estatura/<estatura>")
+def estatura(estatura):
+  resultados=base[base["estatura"]==estatura]
+  resultados=float(resultados)
+  return resultados
+
+print(estatura)
