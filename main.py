b7864fe556277b3a3975393d5234cc11de7316d7
laalop <168581789+laalop@users.noreply.github.com>
2024-05-03 08:03:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,28 +5,29 @@ app=Flask(__name__)
 
 base=pd.read_excel("perros.xlsx")
 
-@app.router("/")
+@app.route("/")
 def Principal():
   return "Esta es una API que te muetra el nombre de perros, raza y estatura"
 
-@app.router("Por_Numero/<Numero>")
+@app.router("/Por_Numero/<Numero>")
 def PorNumero(Numero):
   Numero=int(Numero)
-  fila=base [base["Numero"]==Nombre]
-  respuesta = f"El Perro {Numero} es de raza {fila.loc[:'raza']}"
+  fila=base [base["Numero"]==Numero]
+  respuesta = f"El Perro {Numero} es de raza {fila.loc[:,'raza']}"
   return respuesta
 
-@app.router("/Por_Tipo/<Tipo>")
+@app.route("/Por_Tipo/<Tipo>")
 def PorTipo(Tipo):
   resultados=base[base["Tipo"]==Tipo]
   resultados=srt(resultados)
   return resultados
 
 @app.router("/Por_Peso/<Peso1>/<Peso2>")
-def PorPeso(Peso):
+def PorPeso(Peso1,Peso2):
   Peso1=float(Peso1)
   Peso2=float(Peso2)
-  resultados=base[(base["Peso"]<Peso1) & (base["Peso"]>Peso2)]
+  resultados=base[(base["Peso"]>Peso1) & (base["Peso"]>Peso2)]
+  return resultados
 
-
-print(PorNumero(2))
+if __name__=="__main__":
+  app.run()
