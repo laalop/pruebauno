0cf3e4fdbd6a3a76098c9008b655e02ee754dd48
laalop <168581789+laalop@users.noreply.github.com>
2024-05-02 23:25:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
-# -*- coding: utf-8 -*-
+pandas
+openpyxl
+import pandas as pd
 from flask import Flask
 
 app=Flask(__name__)
 
-@app.route("/")
-def Hola_mundo():
-  return "Hola a todos"
+base=pd.read_excel("perros.xlsx")
 
-if __name__=="__main__":
-  app.run()
+@app.router("/")
+def Principal():
+  return "Esta es una API que te muetra el nombre de perros, raza y estatura"
+
+@app.router("Por_Numero/<Numero>")
+def PorNumero(Numero):
+  Numero=int(Numero)
+  fila=base [base["Numero"]==Nombre]
+  respuesta = f"El Perro {Numero} es de raza {fila.loc[:'raza']}"
+  return respuesta
+
+@app.router("/Por_Tipo/<Tipo>")
+def PorTipo(Tipo):
+  resultados=base[base["Tipo"]==Tipo]
+  resultados=srt(resultados)
+  return resultados
+
+@app.router("/Por_Peso/<Peso1>/<Peso2>")
+def PorPeso(Peso):
+  Peso1=float(Peso1)
+  Peso2=float(Peso2)
+  resultados=base[(base["Peso"]<Peso1) & (base["Peso"]>Peso2)]
+
+
+print(PorNumero(2))
