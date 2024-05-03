a9723ec966cf48c7d590c4ffa50457415f72c674
laalop <168581789+laalop@users.noreply.github.com>
2024-05-03 08:16:26 -0600

    Update main.py

--- main.py
+++ main.py
@@ -12,22 +12,9 @@ def Principal():
 @app.route("/Por_Numero/<Numero>")
 def PorNumero(Numero):
   Numero=int(Numero)
-  fila=base [base["Numero"]==Numero]
-  respuesta = f"El Perro {Numero} es de raza {fila.loc[:,'raza']}"
+  fila=base[base["Numero"]==Numero]
+  respuesta=f"El Perro {Numero} es de raza {fila.loc[:,'raza']}"
   return respuesta
-
-@app.route("/Por_Tipo/<Tipo>")
-def PorTipo(Tipo):
-  resultados=base[base["Tipo"]==Tipo]
-  resultados=srt(resultados)
-  return resultados
-
-@app.route("/Por_Peso/<Peso1>/<Peso2>")
-def PorPeso(Peso1,Peso2):
-  Peso1=float(Peso1)
-  Peso2=float(Peso2)
-  resultados=base[(base["Peso"]>Peso1) & (base["Peso"]>Peso2)]
-  return resultados
 
 if __name__=="__main__":
   app.run()
